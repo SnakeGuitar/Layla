075a1aab79865dc463eb0da17704a401097f17ab
CoronelTocino1 <35902658+Foforfr0@users.noreply.github.com>
2026-03-06 18:13:22 -0600

    feat: FormatData added

---
+++ src/client-web/Helpers/FormatDate.cs
@@ -0,0 +1,10 @@
+namespace client_web.Helpers;
+
+public static class FormatData
+{
+    public static string FormatDateToString(DateTime date)
+    {
+        var months = new[] { "ene", "feb", "mar", "abr", "may", "jun", "jul", "ago", "sep", "oct", "nov", "dic" };
+        return $"{date.Day} {months[date.Month - 1]}. {date.Year}";
+    }
+}
