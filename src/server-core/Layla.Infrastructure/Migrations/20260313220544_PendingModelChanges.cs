a5b4de227c3e9e2544d83df19e3e60725ae1f61a
SnakeGuitar <snakersito08@gmail.com>
2026-03-14 22:20:55 -0600

    feat: add db migrations

---
+++ src/server-core/Layla.Infrastructure/Migrations/20260313220544_PendingModelChanges.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Layla.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class PendingModelChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsPublic",
+                table: "Projects",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "ProjectId",
+                table: "AspNetUsers",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_ProjectId",
+                table: "AspNetUsers",
+                column: "ProjectId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_Projects_ProjectId",
+                table: "AspNetUsers",
+                column: "ProjectId",
+                principalTable: "Projects",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_Projects_ProjectId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_ProjectId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "IsPublic",
+                table: "Projects");
+
+            migrationBuilder.DropColumn(
+                name: "ProjectId",
+                table: "AspNetUsers");
+        }
+    }
+}
