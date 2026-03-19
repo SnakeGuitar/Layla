88af52ece74d32bce6768fa361922de34b038ece
SnakeGuitar <snakersito08@gmail.com>
2026-03-19 15:14:57 -0600

    fix: update migrations

---
+++ src/server-core/Layla.Infrastructure/Migrations/20260315234106_UpdateProjectModel_20260315174059.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Layla.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateProjectModel_20260315174059 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Projects_IsPublic",
+                table: "Projects",
+                column: "IsPublic");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Projects_IsPublic",
+                table: "Projects");
+        }
+    }
+}
